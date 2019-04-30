d6713632b83734e637fd45acdc52fe49a037d25d
Ming-Sheng,Lyu <sam850118sam@gmail.com>
2019-04-30 20:45:53 +0800

    init

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)  
 def handle_text_message(event):                  
     msg = event.message.text 
-
+    
     
     line_bot_api.reply_message(
         event.reply_token,
